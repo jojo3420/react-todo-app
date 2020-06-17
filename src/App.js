bf55c9ff523d737e53170a03e8575192a3efc99b
jojo3420 <jjjhhhvvv@gmail.com>
2020-06-17 18:54:29 +0900

    성능 최적화  - useReducer + React.memo()  - useReducer 의 장점은 한곳에서 상태를 관리할 수 있고 외부 함수로 추출 할 수도 있어도 좋다. 그리고 dispatch 함수만 호출하면 되니깐 컴포넌트 로직이 단순해진다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
 import './App.css';
 import Template from 'components/Template/Template';
 import TodoInput from 'components/TodoInput/TodoInput';
 import TodoList from 'components/TodoList/TodoList';
 
+
+
+const reducer = (todoList, action) => {
+	switch (action.type) {
+		case 'ADD':
+			return todoList.concat({
+				id: action.id,
+				text: action.text,
+				checked: false
+			});
+		case 'REMOVE':
+			return todoList.filter(todo => todo.id !== action.id);
+		case 'MODIFY':
+			return todoList.map(todo => todo.id === action.id ? { ...todo, checked: !todo.checked} : todo)
+		default:
+			return todoList;
+	}
+};
+
 function App() {
 	const [text, setText] = useState('');
-	const [todoList, setTodoList] = useState(createSampleData);
+	const [todoList, dispatch] = useReducer(reducer, [], createSampleData);
 	const idRef = useRef(3000);
 
 	const handleChange = useCallback(e => {
@@ -17,27 +36,34 @@ function App() {
 	const handleInsert = useCallback(e => {
 		e.preventDefault();
 		if (text) {
-			// 함수형 업데이트로 변경
-			setTodoList(todoList => todoList.concat(
-				{ id: idRef.current++, text, checked: false }
-				));
+			// dispatch
+			dispatch({
+				type: 'ADD',
+				id: idRef.current++,
+				text,
+			})
 			setText('');
 		}
 	}, [text]);
 
 	const handleRemove = useCallback(id => {
 		if (id) {
-			// 함수형 업데이트 코드로 변경
-			setTodoList(todoList => todoList.filter(todo => todo.id !== id));
+			// dispatch
+			dispatch({
+				type: 'REMOVE',
+				id,
+			});
 			return;
 		}
 		throw new Error('삭제할 id가 없습니다.' + id);
 	}, []); // todoList 의존성 제거
 
 	const handleChecked = useCallback(id => {
-		// 함수형 업데이트 코드로 변경
-		setTodoList(todoList =>
-			todoList.map(todo => todo.id === id ? { ...todo, checked: !todo.checked}: todo))
+		// dispatch
+		dispatch({
+			type: 'MODIFY',
+			id,
+		})
 	}, []); // todoList 의존성 제거
 
 
